02b14853835c83e3d8c0fcd572fa130a774431cf
SenyshynM <73256299+SenyshynM@users.noreply.github.com>
2022-04-14 14:42:31 +0300

    Add files via upload

---
+++ lab1/ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,101 @@
+﻿#include <iostream>
+
+using namespace std;
+
+void printch(int* x)
+{
+	for (int i = 0; i < 8; i++)
+		cout << x[i];
+	cout << endl;
+}
+
+void oper_or(int* fir, int* sec, int* operation)
+{
+	for (int i = 0; i < 8; i++)
+	{
+		if (fir[i] == 1 || sec[i] == 1)
+			operation[i] = 1;
+		else
+			operation[i] = 0;
+	}
+}
+
+void oper_and(int* fir, int* sec, int* operation)
+{
+	for (int i = 0; i < 8; i++)
+	{
+		if (fir[i] == 1 && sec[i] == 1)
+			operation[i] = 1;
+		else
+			operation[i] = 0;
+	}
+}
+
+void oper_xor(int* fir, int* sec, int* operation)
+{
+	for (int i = 0; i < 8; i++)
+	{
+		if (fir[i] == 1 && sec[i] == 1 || fir[i] == 0 && sec[i] == 0)
+			operation[i] = 0;
+		else
+			operation[i] = 1;
+	}
+}
+
+void zavd2()
+{
+	cout << endl << endl << "Zavd 2" << endl;
+	const int n = 8;
+	int fir[n] = { 1, 1, 1, 0, 0, 0, 1, 0 };
+	int sec[n] = { 1, 0, 1, 0, 1, 0, 1, 0 };
+	int operation[n];
+	printch(fir);
+	printch(sec);
+	cout << endl << "or:" << endl;
+	oper_or(fir, sec, operation);
+	printch(operation);
+	cout << endl << "and:" << endl;
+	oper_and(fir, sec, operation);
+	printch(operation);
+	cout << endl << "xor:" << endl;
+	oper_xor(fir, sec, operation);
+	printch(operation);
+}
+
+void zavd1()
+{
+	cout << "Zavd 1" << endl;
+	bool p, q;
+	cout << "p: "; cin >> p;
+	cout << "q: ";  cin >> q;
+	cout << endl;
+	if (p == true && q == true) // кон’юнкції
+		cout << "p and q: true" << endl;
+	else cout << "p and q: false" << endl;
+
+	if (p == true || q == true) // диз’юнкції
+		cout << "p or q: true" << endl;
+	else cout << "p or q: false" << endl;
+
+	if (p == true ^ q == true) // альтернативного або
+		cout << "p xor q: true" << endl;
+	else cout << "p xor q: false" << endl;
+
+	if (p == true && q == false) // імплікації p -> q
+		cout << "p -> q: true" << endl;
+	else cout << "p -> q: false" << endl;
+
+	if (p == false && q == true) // імплікації q -> p
+		cout << "q -> p: true" << endl;
+	else cout << "q -> p: false" << endl;
+
+	if ((!p == true || q == true) && (p == true || !q == true)) //еквіваллентності p ~ q
+		cout << "p ~ q: true" << endl;
+	else cout << "p ~ q: false" << endl;
+}
+
+int main()
+{	
+	zavd1(); // завдання 1
+	zavd2(); // завдання 2
+}
---
+++ lab2/lab2.cpp
@@ -0,0 +1,57 @@
+﻿#include <iostream>
+
+using namespace std;
+
+void op_pq(int* p, int* q, int* pq, const int n)
+{
+	for (int i = 0; i < n; i++)
+	{
+		if (p[i] != 1 || q[i] == 1)
+			pq[i] = 1;
+		else pq[i] = 0;
+	}
+}
+
+void op_pr(int* p, int* r, int* pr, const int n)
+{
+	for (int i = 0; i < n; i++)
+	{
+		if (p[i] != 1 || r[i] == 1)
+			pr[i] = 1;
+		else pr[i] = 0;
+	}
+}
+
+void op_pqr(int* pq, int* pr, int* pqr, const int n)
+{
+	for (int i = 0; i < n; i++)
+	{
+		if (pq[i] == 1 && pr[i] == 1)
+			pqr[i] = 1;
+		else
+			pqr[i] = 0;
+	}
+}
+
+int main()
+{
+	const int n = 8;
+	int p[n] = { 0,0,0,0,1,1,1,1 };
+	int q[n] = { 0,1,0,1,0,1,0,1 };
+	int r[n] = { 0,0,1,1,0,0,1,1 };
+	int pq[n];
+	int pr[n];
+	int pqr[n];
+	op_pq(p, q, pq, n);
+	op_pr(p, r, pr, n);
+	op_pqr(pq, pr, pqr, n);
+	cout << "           (p -> q) ^ (p -> r)" << endl << endl;;
+	cout << "----------------------------------------------------" << endl;
+	cout << "| p | q | r | p -> q | p - r | (p -> q) ^ (p -> r) | " << endl;
+	cout << "----------------------------------------------------" << endl;
+	for (int i = 0; i < n; i++)
+	{
+		cout << "| " << p[i] << " | " << q[i] << " | " << r[i] << " |    " << pq[i] << "   |   " << pr[i] << "   |          " << pqr[i] << "          |" << endl;
+	}
+	cout << "----------------------------------------------------" << endl;
+}
